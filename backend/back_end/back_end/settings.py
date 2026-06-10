e49ebdd0068c919aa5e85017a5da80c6b56dff08
abdullahwahidi199 <wahidia53@gmail.com>
2026-06-10 08:43:44 +0430

    added postgress to settings

--- backend/back_end/back_end/settings.py
+++ backend/back_end/back_end/settings.py
@@ -93,16 +93,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'back_end.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cms_db',
+        'USER':'cms_user',
+        'PASSWORD':'123abc,?',
+        'HOST':'localhost',
+        'PORT':'5432'
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
