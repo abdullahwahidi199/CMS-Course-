abb8e7cd98d2ecbe6ada1a7b7e14772afd502d2d
abdullahwahidi199 <wahidia53@gmail.com>
2026-07-16 11:44:14 +0430

    db altered

--- backend/back_end/back_end/settings.py
+++ backend/back_end/back_end/settings.py
@@ -124,12 +124,12 @@ DATABASES = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
